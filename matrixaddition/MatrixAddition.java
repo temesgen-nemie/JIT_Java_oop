f1dedb2473837f7d472548cba3cd22fffec4fef1
Temesgen nemie <119539072+temesgen-nemie@users.noreply.github.com>
2023-01-17 11:53:27 +0300

    Add files via upload

---
+++ matrixaddition/MatrixAddition.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matrixaddition;
+
+/**
+ *
+ * @author Fasil
+ */
+public class MatrixAddition {
+public static void main(String args[]){  
+
+//creating two matrices    
+int a[][]={{4,9,4},{77,4,3},{3,4,5}};    
+int b[][]={{7,8,4},{90,4,3},{1,2,4}};    
+    
+//creating another matrix to store the sum of two matrices    
+int c[][]=new int[3][3];  //3 rows and 3 columns  
+    
+//adding and printing addition of 2 matrices    
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+c[i][j]=a[i][j]+b[i][j];    //use - for subtraction  
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println();//new line    
+}    
+}}
+
